1d58bde7ccc53c62cb8b58dc6b9bfb69740f0290
Hazley Jarquin <hazleyjarquin03@gmail.com>
2024-06-10 02:27:26 -0600

    add new dark mode color to components

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,7 @@ import { NewCard } from './components/NewCard';
 import { Button } from './components/ui/button';
 import { Card } from './components/ui/card';
 import { ToggleButton } from './components/ToggleButton';
+import { Graph } from './components/Graph';
+import { DataTable } from './components/DataTable';
 
-export { NewButton, NewCard, Button, Card, ToggleButton };
+export { NewButton, NewCard, Button, Card, ToggleButton, DataTable, Graph };
--- src/stories/NewButton/newButtonContent.stories.ts
+++ src/stories/NewButton/newButtonContent.stories.ts
@@ -16,6 +16,7 @@ type Story = StoryObj<typeof meta>;
 export const content: Story = {
   args: {
     title: 'NewButton',
+    variant: 'ghost',
   },
   //   argTypes: {
   //     isSelected: { table: { disable: true } },
